bf793f8ed127e3e1bf73159746aeedb5cbef8024
daneriulisesjulian <ejemploweb26@gmail.com>
2026-07-28 22:31:40 -0300

    Actualizar tarifas y quitar motomensajeria

--- server.js
+++ server.js
@@ -26,14 +26,7 @@ app.post("/api/shipping/quote", async (req, res) => {
       carrierDescription: "Correo Argentino",
       serviceDescription: "Envío Estándar",
       deliveryEstimate: "3-5 días",
-      totalPrice: 13000
-    },
-    {
-      carrierDescription: "Motomensajería",
-      serviceDescription: "Envío Express (En el día)",
-      deliveryEstimate: "24hs",
-      totalPrice: 0,
-      customLabel: "A coordinar por WhatsApp"
+      totalPrice: 10000
     }
   ];
 
