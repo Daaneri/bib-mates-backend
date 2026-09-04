c79b3df25ebbf88c1eb932ad6065319f25ebc4b3
daneriulisesjulian <ejemploweb26@gmail.com>
2026-09-04 19:15:36 -0300

    Fix login: usa BACKEND_URL en vez de ruta relativa

--- crear-admin.js
+++ crear-admin.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
 // --------- EDITÁ ESTOS DOS VALORES ---------
-const ADMIN_EMAIL = "ivanezequieljure1997@gmail.com";
+const ADMIN_EMAIL = "ivanezequieljure1997@hotmail.com";
 const ADMIN_PASSWORD = "AdminMatesBIB";
 // --------------------------------------------
 
